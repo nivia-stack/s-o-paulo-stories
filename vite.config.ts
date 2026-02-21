cc7200718ccb83a286b9deef53bde61a36a87226
Nivia Alves <niviafalves@hotmail.com>
2026-02-21 15:07:59 -0300

    Configuraçao para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/s-o-paulo-stories/',
   server: {
     host: "::",
     port: 8080,
